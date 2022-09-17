483b26392b3cce11d26d08898a095de594f8d1e9
MXZERO <mxzeromxzero6@gmail.com>
2022-09-17 14:53:52 +0700

    Fix N+1

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    protected $with = ["movie","studio"];
+
     public function studio()
     {
         $this->belongsTo(Studio::class);

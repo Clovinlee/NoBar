2d984c9f4d3649231e9ecabc4819c45129da16bb
MXZERO <mxzeromxzero6@gmail.com>
2022-09-25 01:14:51 +0700

    UI Booking seat
    UI, and seat click. Including getting schedule from Database, and other stuff from DB

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -9,10 +9,12 @@ class Branch extends Model
 {
     use HasFactory;
 
-    protected $with = ["studio"];
-    
     public function studio()
     {
         return $this->hasMany(Studio::class);
+    }
+
+    public function schedules(){
+        return $this->hasMany(Schedule::class);
     }
 }

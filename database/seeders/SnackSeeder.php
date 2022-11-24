24491912b850be65f3deafdf99ab7450dc3f4776
edwardpatrick34 <edwardpatrick34@gmail.com>
2022-11-24 21:57:59 +0700

    db(snack)
    pembuatan data dummy snack dengan seeder

--- database/seeders/SnackSeeder.php
+++ database/seeders/SnackSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Snack;
 
 class SnackSeeder extends Seeder
 {
@@ -14,6 +15,60 @@ class SnackSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Snack::create([
+            "nama" => "Coca - cola",
+            "harga" => "12000",
+            "tipe" => "beverage",
+            "foto" => "cola.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Lemon tea",
+            "harga" => "12000",
+            "tipe" => "beverage",
+            "foto" => "lemon.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Orange juice",
+            "harga" => "12000",
+            "tipe" => "beverage",
+            "foto" => "orange.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Aqua",
+            "harga" => "10000",
+            "tipe" => "beverage",
+            "foto" => "aqua.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Popcorn",
+            "harga" => "10000",
+            "tipe" => "food",
+            "foto" => "popcorn.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Hotdog",
+            "harga" => "15000",
+            "tipe" => "food",
+            "foto" => "hotdog.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "French-fries",
+            "harga" => "12000",
+            "tipe" => "food",
+            "foto" => "fries.jpg",
+        ]);
+
+        Snack::create([
+            "nama" => "Burger",
+            "harga" => "15000",
+            "tipe" => "food",
+            "foto" => "burger.jpg",
+        ]);
     }
 }

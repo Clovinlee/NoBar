5ab9f536e184ac20a3ffa1ee8290c53e2ddd6ff0
EdwardPatrick34 <edwardpatrick34@gmail.com>
2022-12-02 19:51:09 +0700

    feat(dashboard admin)
    pembuatan dashboard admin (incomplete)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,11 @@
       <section class="mb-4">
         <h1 style="color: black">Dashboard</h1>
         
+        <h2 style="color: black">Snack yang ditambahkan hari ini :</h2>
+        @foreach($data as $d){
+          
+        }
+        @endforeach
       </section> 
     </div>
   </main>

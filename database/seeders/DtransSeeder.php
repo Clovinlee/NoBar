23ace78b4906f764621f39bc3e28c1e2a3ce52ad
estergbriel <esther_g20@mhs.istts.ac.id>
2022-12-05 22:45:24 +0700

    seeder

--- database/seeders/DtransSeeder.php
+++ database/seeders/DtransSeeder.php
@@ -16,21 +16,143 @@ class DtransSeeder extends Seeder
     public function run()
     {
         //
-        // Dtrans::create([
-        //     "htrans_id"=>1,
-        //     "chair"=>1
-        // ]);
-        // Dtrans::create([
-        //     "htrans_id"=>1,
-        //     "chair"=>3
-        // ]);
-        // Dtrans::create([
-        //     "htrans_id"=>2,
-        //     "chair"=>3
-        // ]);
-        // Dtrans::create([
-        //     "htrans_id"=>3,
-        //     "chair"=>2
-        // ]);
+        Dtrans::create([
+            "htrans_id"=>1,
+            "chair"=>"A1"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>1,
+            "chair"=>"A2"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>1,
+            "chair"=>"A3"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>1,
+            "chair"=>"A4"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>1,
+            "chair"=>"A5"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>2,
+            "chair"=>"B1"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>2,
+            "chair"=>"B2"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>3,
+            "chair"=>"C1"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>4,
+            "chair"=>"D1"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>4,
+            "chair"=>"D2"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>5,
+            "chair"=>"E1"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>5,
+            "chair"=>"E2"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>5,
+            "chair"=>"E3"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>5,
+            "chair"=>"E4"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F6"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F7"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F8"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F9"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F10"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>6,
+            "chair"=>"F11"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>7,
+            "chair"=>"G1"
+        ]);
+        Dtrans::create([
+            "htrans_id"=>8,
+            "chair"=>"H5"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>8,
+            "chair"=>"H6"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>9,
+            "chair"=>"I1"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>9,
+            "chair"=>"I2"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>9,
+            "chair"=>"I3"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>10,
+            "chair"=>"J11"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>10,
+            "chair"=>"J12"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>10,
+            "chair"=>"J13"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>10,
+            "chair"=>"J14"
+        ]);
+        
+        Dtrans::create([
+            "htrans_id"=>11,
+            "chair"=>"A10"
+        ]);
     }
 }
--- database/seeders/DtranssnackSeeder.php
+++ database/seeders/DtranssnackSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dtranssnack;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,127 @@ class DtranssnackSeeder extends Seeder
     public function run()
     {
         //
+        Dtranssnack::create([
+            "htranssnack_id"=>1,
+            "snack_id"=>1,
+            "harga"=>12000,
+            "qty"=>5
+        ]);
+        Dtranssnack::create([
+            "htranssnack_id"=>1,
+            "snack_id"=>5,
+            "harga"=>10000,
+            "qty"=>4
+        ]);
+        Dtranssnack::create([
+            "htranssnack_id"=>2,
+            "snack_id"=>2,
+            "harga"=>12000,
+            "qty"=>10
+        ]);
+        Dtranssnack::create([
+            "htranssnack_id"=>2,
+            "snack_id"=>3,
+            "harga"=>12000,
+            "qty"=>5
+        ]);
+        Dtranssnack::create([
+            "htranssnack_id"=>2,
+            "snack_id"=>4,
+            "harga"=>10000,
+            "qty"=>2
+        ]);
+        Dtranssnack::create([
+            "htranssnack_id"=>3,
+            "snack_id"=>3,
+            "harga"=>12000,
+            "qty"=>5
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>3,
+            "snack_id"=>7,
+            "harga"=>12000,
+            "qty"=>5
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>3,
+            "snack_id"=>6,
+            "harga"=>15000,
+            "qty"=>6
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>3,
+            "snack_id"=>8,
+            "harga"=>15000,
+            "qty"=>6
+        ]);
+
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>4,
+            "snack_id"=>8,
+            "harga"=>15000,
+            "qty"=>10
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>4,
+            "snack_id"=>6,
+            "harga"=>15000,
+            "qty"=>10
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>4,
+            "snack_id"=>5,
+            "harga"=>10000,
+            "qty"=>5
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>4,
+            "snack_id"=>4,
+            "harga"=>10000,
+            "qty"=>5
+        ]);
+
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>5,
+            "snack_id"=>1,
+            "harga"=>12000,
+            "qty"=>10
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>5,
+            "snack_id"=>2,
+            "harga"=>12000,
+            "qty"=>10
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>5,
+            "snack_id"=>3,
+            "harga"=>12000,
+            "qty"=>10
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>6,
+            "snack_id"=>1,
+            "harga"=>15000,
+            "qty"=>8
+        ]);
+        
+        Dtranssnack::create([
+            "htranssnack_id"=>5,
+            "snack_id"=>5,
+            "harga"=>10000,
+            "qty"=>2
+        ]);
     }
 }

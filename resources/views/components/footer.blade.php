4e0195731629fc20e5319987c01d6592d9ce8e3f
Kevin Syahputra <kevin_s20@mhs.istts.ac.id>
2022-12-07 22:17:09 +0700

    footer
    sosMed

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -44,10 +44,10 @@
                     <div class="col-md-5">
                         <div class="footer-social">
                             <ul>
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="https://id-id.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="https://www.pinterest.com/"><i class="fab fa-pinterest-p"></i></a></li>
+                                <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
                             </ul>
                         </div>
                     </div>
